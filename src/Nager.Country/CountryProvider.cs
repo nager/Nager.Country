1ab816e8f800df99bbc0424fe5d6b3f925a4fcd3
Tino Hager <tino.hager@nager.at>
2019-01-26 15:57:32 +0100

    Add first interface layout to access the country data

---
+++ src/Nager.Country/CountryProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nager.Country
+{
+    public class CountryProvider : ICountryProvider
+    {
+        public ICountryInfo GetCountry(string alpha2or3Code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICountryInfo GetCountry(Alpha2Code alpha2Code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICountryInfo GetCountry(Alpha3Code alpha3Code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Nager.Country/ICountryProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Nager.Country
+{
+    public interface ICountryProvider
+    {
+        ICountryInfo GetCountry(string alpha2or3Code);
+
+        ICountryInfo GetCountry(Alpha2Code alpha2Code);
+        ICountryInfo GetCountry(Alpha3Code alpha3Code);
+    }
+}
