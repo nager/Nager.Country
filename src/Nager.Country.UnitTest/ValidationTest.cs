de2660a16d87819ff2cae1d5aa4f416855607ef5
Jean Voaduloor <kprudhomm@gmail.com>
2019-04-24 11:33:11 +0200

    - add new helpers method in CountryProvider to get all country list and translated name from language code (string or enum values) and CultureInfo object directly.

--- src/Nager.Country.UnitTest/ValidationTest.cs
+++ src/Nager.Country.UnitTest/ValidationTest.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nager.Country;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Nager.Date.UnitTest
@@ -66,6 +67,92 @@ namespace Nager.Date.UnitTest
             if (countryInfo == null)
             {
                 Assert.Fail($"Found country not {countryCode}");
+            }
+        }
+
+        public void GetCountries()
+        {
+            ICountryProvider countryProvider = new CountryProvider();
+            var countries = countryProvider.GetCountries();
+
+            foreach (var countryCode in (Alpha2Code[])Enum.GetValues(typeof(Alpha2Code)))
+            {
+                if(countries.Any(x => x.Alpha2Code == countryCode) == false)
+                {
+                    Assert.Fail($"No country in all countries list for country code {countryCode}");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CheckAllCulture()
+        {
+            ICountryProvider countryProvider = new CountryProvider();
+            CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            foreach (var countryCode in (Alpha2Code[])Enum.GetValues(typeof(Alpha2Code)))
+            {
+                var countryInfo = countryProvider.GetCountry(countryCode);
+                if (countryInfo == null)
+                    continue;
+
+                var expectedLanguages = countryInfo.Translations.Select(x => x.LanguageCode).ToList();
+                foreach (var culture in cultures)
+                {
+                    bool expectResult = false;
+                    if (Enum.TryParse(culture.TwoLetterISOLanguageName, true, out LanguageCode code) == true)
+                    {
+                        expectResult = expectedLanguages.Any(x => x == code);
+                    }
+
+                    var translatedCountryName = countryProvider.GetCountryTranslatedName(countryCode, culture);
+                    if(expectResult == true && String.IsNullOrWhiteSpace(translatedCountryName) == true)
+                    {
+                        Assert.Fail($"A result was expected but there was no translated country name found for {countryCode} and culture {culture.Name} (language {culture.TwoLetterISOLanguageName})");
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        [DataRow(LanguageCode.EN)]
+        [DataRow(LanguageCode.AA)]
+        [DataRow(LanguageCode.DE)]
+        [DataRow(LanguageCode.RU)]
+        public void CheckAllCultureWithDefault(LanguageCode defaultLanguageCode)
+        {
+            ICountryProvider countryProvider = new CountryProvider();
+            CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            foreach (var countryCode in (Alpha2Code[])Enum.GetValues(typeof(Alpha2Code)))
+            {
+                var countryInfo = countryProvider.GetCountry(countryCode);
+                if (countryInfo == null)
+                    continue;
+
+                var expectedLanguages = countryInfo.Translations.Select(x => x.LanguageCode).ToList();
+                foreach (var culture in cultures)
+                {
+                    bool expectResult = false;
+                    if (Enum.TryParse(culture.TwoLetterISOLanguageName, true, out LanguageCode code) == true)
+                    {
+                        expectResult = expectedLanguages.Any(x => x == code);
+                    }
+
+                    var translatedCountryName = countryProvider.GetCountryTranslatedName(countryCode, culture, defaultLanguageCode);
+                    if (expectResult == true && String.IsNullOrWhiteSpace(translatedCountryName) == true)
+                    {
+                        Assert.Fail($"A result was expected but there was no translated country name found for {countryCode} and culture {culture.Name} (language {culture.TwoLetterISOLanguageName})");
+                    }
+                    else
+                    {
+                        expectResult = expectedLanguages.Any(x => x == defaultLanguageCode);
+                        if (expectResult == true && String.IsNullOrWhiteSpace(translatedCountryName) == true)
+                        {
+                            Assert.Fail($"A result was expected for default value but there was no translated country name found for {countryCode} and default language code {defaultLanguageCode}");
+                        }
+                    }
+                }
             }
         }
     }
--- src/Nager.Country/ICountryProvider.cs
+++ src/Nager.Country/ICountryProvider.cs
@@ -1,10 +1,30 @@
-﻿namespace Nager.Country
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Nager.Country
 {
     public interface ICountryProvider
     {
-        ICountryInfo GetCountry(string alpha2or3Code);
+        IEnumerable<ICountryInfo> GetCountries();
 
+        ICountryInfo GetCountry(string alpha2or3Code);
         ICountryInfo GetCountry(Alpha2Code alpha2Code);
         ICountryInfo GetCountry(Alpha3Code alpha3Code);
+
+        string GetCountryTranslatedName(string alpha2or3Code, LanguageCode languageCode);
+        string GetCountryTranslatedName(Alpha2Code alpha2Code, LanguageCode languageCode);
+        string GetCountryTranslatedName(Alpha3Code alpha3Code, LanguageCode languageCode);
+
+        string GetCountryTranslatedName(string alpha2or3Code, string languageCode);
+        string GetCountryTranslatedName(Alpha2Code alpha2Code, string languageCode);
+        string GetCountryTranslatedName(Alpha3Code alpha3Code, string languageCode);
+
+        string GetCountryTranslatedName(string alpha2or3Code, CultureInfo culture);
+        string GetCountryTranslatedName(Alpha2Code alpha2Code, CultureInfo culture);
+        string GetCountryTranslatedName(Alpha3Code alpha3Code, CultureInfo culture);
+
+        string GetCountryTranslatedName(string alpha2or3Code, CultureInfo culture, LanguageCode defaultLanguageCode);
+        string GetCountryTranslatedName(Alpha2Code alpha2Code, CultureInfo culture, LanguageCode defaultLanguageCode);
+        string GetCountryTranslatedName(Alpha3Code alpha3Code, CultureInfo culture, LanguageCode defaultLanguageCode);
     }
 }
