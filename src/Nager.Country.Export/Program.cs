e018bd92dafcbe0e9ef8f46170cd897f317ec190
Tino Hager <tino.hager@nager.at>
2020-12-09 21:07:48 +0100

    Export country infos as json

---
+++ src/Nager.Country.Export/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Nager.Country.Export
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exportDirectory = "export";
+
+            if (!Directory.Exists(exportDirectory))
+            {
+                Directory.CreateDirectory(exportDirectory);
+            }
+
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters =
+                {
+                    new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+                },
+                WriteIndented = true
+            };
+
+            var countryProvider = new CountryProvider();
+            var countries = countryProvider.GetCountries();
+            foreach (var country in countries)
+            {
+                var jsonData = JsonSerializer.SerializeToUtf8Bytes(country, jsonSerializerOptions);
+                File.WriteAllBytesAsync($"{exportDirectory}/{country.Alpha2Code}.json", jsonData).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
