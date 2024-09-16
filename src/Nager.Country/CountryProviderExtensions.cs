9a5b082727a5c5dfcc339156b3fc9f6b9255d02b
Christoph Potas <christoph286@googlemail.com>
2024-09-16 11:55:27 +0200

    country info by region
    + add extension to get countries by region or sub region

---
+++ src/Nager.Country/CountryProviderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Nager.Country;
+
+public static class CountryProviderExtensions
+{
+	public static IEnumerable<ICountryInfo> GetCountriesByRegion( this ICountryProvider provider, Region region )
+	{
+		return provider.GetCountries().Where( x => x.Region == region );
+	}
+
+	public static IEnumerable<ICountryInfo> GetCountriesBySubRegion( this ICountryProvider provider,
+		SubRegion subRegion )
+	{
+		return provider.GetCountries().Where( x => x.SubRegion == subRegion );
+	}
+}
