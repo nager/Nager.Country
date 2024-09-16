c80c48d79bf0cad8c4c9556a95a949fdb8105d71
Christoph Potas <christoph286@googlemail.com>
2024-09-16 11:54:52 +0200

    alpha code conversions
    + add extension to convert alpha code 2 to alpha code 3 and vice versa

---
+++ src/Nager.Country/AlphaCodeConversionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Nager.Country;
+
+public static class AlphaCodeConversionExtensions
+{
+	public static Alpha3Code ToAlpha3Code( this Alpha2Code code )
+	{
+		return (Alpha3Code)code;
+	}
+
+	public static Alpha2Code ToAlpha2Code( this Alpha3Code code )
+	{
+		return (Alpha2Code)code;
+	}
+}
